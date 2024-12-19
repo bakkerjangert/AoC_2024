51ccbdfc177d70a770d03b568bf5c19cf3c8cb0f
903774 <bakkerjangert@hotmail.com>
2024-12-19 09:20:38 +0100

    Day 19 solved

--- Day 19/Day 19.py
+++ Day 19/Day 19.py
@@ -11,20 +11,6 @@ def can_build_string(target_string, substrings):
                         dp[i] += dp[i - len(substring)]
     return dp[-1]
 
-def can_build_string_own(target_string, substrings):
-    dp = [0] * (len(target_string) + 1)
-    dp[0] = 1  # Empty string can be built
-    # Check for each position i if the string[:i] can be built up by adding one of the substrings
-    # keep counters for possible number of buildups
-
-    for i in range(1, len(target_string) + 1):
-        for substring in substrings:
-            if i >= len(substring):
-                if dp[i - len(substring)] > 0:
-                    if target_string[i - len(substring):i] == substring:
-                        dp[i] += dp[i - len(substring)]
-    return dp[-1]
-
 file = 'input.txt'
 # file = 'example_01.txt'
 
